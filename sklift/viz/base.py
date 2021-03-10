9f9d5be0ccf64e7f33db403bf0df969ea91cab46
Anwar Babu <53989149+anwarbabukm@users.noreply.github.com>
2021-03-11 01:12:19 +0530

    Update base.py

--- sklift/viz/base.py
+++ sklift/viz/base.py
@@ -40,18 +40,18 @@ def plot_uplift_preds(trmnt_preds, ctrl_preds, log=False, bins=100):
 
     fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(20, 7))
     axes[0].hist(
-        trmnt_preds, bins=bins, alpha=0.3, color='b', label='Treated', histtype='stepfilled')
+        trmnt_preds, bins=bins, alpha=0.3, color='b', label='Treated', histtype='stepfilled',range=(0.1,0.1))
     axes[0].set_ylabel('Probability hist')
     axes[0].legend()
     axes[0].set_title('Treatment predictions')
 
     axes[1].hist(
-        ctrl_preds, bins=bins, alpha=0.5, color='y', label='Not treated', histtype='stepfilled')
+        ctrl_preds, bins=bins, alpha=0.5, color='y', label='Not treated', histtype='stepfilled',range=(0.1,0.1))
     axes[1].legend()
     axes[1].set_title('Control predictions')
 
     axes[2].hist(
-        trmnt_preds - ctrl_preds, bins=bins, alpha=0.5, color='green', label='Uplift', histtype='stepfilled')
+        trmnt_preds - ctrl_preds, bins=bins, alpha=0.5, color='green', label='Uplift', histtype='stepfilled',range=(0.1,0.1))
     axes[2].legend()
     axes[2].set_title('Uplift predictions')
 
